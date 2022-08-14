778de5b984447b0742e1c461dea54700f0886370
JoaoSchmitz <jmatheus12349@gmail.com>
2022-08-14 18:14:04 -0300

    :sparkles: criando a parte da logo na landing page

--- pokeranking/src/Pages/Home/index.jsx
+++ pokeranking/src/Pages/Home/index.jsx
@@ -1,8 +1,14 @@
+import LogoDisplay from "../../Components/LogoDisplay";
 import PokemonDisplay from "../../Components/PokemonDisplay";
 import "./style.css";
 
 function Home() {
-  return <PokemonDisplay />;
+  return (
+    <>
+      <LogoDisplay />
+      <PokemonDisplay />
+    </>
+  );
 }
 
 export default Home;
