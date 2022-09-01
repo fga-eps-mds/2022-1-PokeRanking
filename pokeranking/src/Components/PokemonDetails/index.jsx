25a63fdb70550e16f035ad5afe126ab99c3a9875
Thiago <thiagocerq21@gmail.com>
2022-09-01 00:41:27 -0300

    :sparkles: adicionando card de detalhes.

---
+++ pokeranking/src/Components/PokemonDetails/index.jsx
@@ -0,0 +1,132 @@
+import { useEffect,useState } from "react";
+import "./style.css";
+import grass from "../../Assets/Elements/Grass.svg";
+import ghost from "../../Assets/Elements/Ghost.svg";
+import flying from "../../Assets/Elements/Flying.svg";
+import water from "../../Assets/Elements/Water.svg";
+import steel from "../../Assets/Elements/Steel.svg";
+import fire from "../../Assets/Elements/Fire.svg";
+import rock from "../../Assets/Elements/Rock.svg";
+import fighting from "../../Assets/Elements/Fighting.svg";
+import psychic from "../../Assets/Elements/Psychic.svg";
+import fairy from "../../Assets/Elements/Fairy.svg";
+import poison from "../../Assets/Elements/Poison.svg";
+import electric from "../../Assets/Elements/Electric.svg";
+import normal from "../../Assets/Elements/Normal.svg";
+import dragon from "../../Assets/Elements/Dragon.svg";
+import ice from "../../Assets/Elements/Ice.svg";
+import dark from "../../Assets/Elements/Dark.svg";
+import ground from "../../Assets/Elements/Ground.svg";
+import bug from "../../Assets/Elements/Bug.svg";
+
+
+const elementsIcons = {
+  grass,ghost,flying,water,steel,fire,rock,fighting,psychic,fairy,poison,electric,normal,dragon,ice,dark,ground,bug,
+}
+
+function PokemonDetails({ opened, setOpened, pokemonInfo }) {
+  const [pokemonDataCard, setPokemonDataCard] = useState(null);
+
+
+  useEffect(() => {
+    if (pokemonInfo){
+      fetch(`http://localhost:5000/criaturas/${pokemonInfo.codigo}`)
+        .then((response) => response.json())
+        .then((data) => {setPokemonDataCard(data[0]); console.log(data[0])});
+    }
+  }, [pokemonInfo]);
+
+  if (!opened || !pokemonDataCard ) return null; 
+
+  return (
+    <div id="modal-braba" className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <span className="close" onClick={() => setOpened(false)}>&times;</span>
+       
+                        
+        </div>
+        <div className="modal-body">
+          {/* interpolar javascript */}
+          <img
+                  className="card-image-modal"
+                  src={pokemonInfo.url_image}
+                  alt={pokemonInfo.name}      
+          />
+          </div>
+
+          <div className="Info">
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <p className="card-title-number"># {pokemonInfo.codigo}</p>
+          <br/>
+          <p className="card-title-name">{pokemonInfo.name}</p>
+          <br/>
+          <div className="card-modal-types">
+            <img
+              className="card-modal-element-icon"
+              src={elementsIcons[pokemonInfo.type_1]}
+              alt={pokemonInfo.type_1}
+              title={pokemonInfo.type_1}
+            />
+
+            {(pokemonInfo.type_2 && pokemonInfo.type_2 !== 'NA') && (
+              <img
+                className="card-modal-element-icon" 
+                src={elementsIcons[pokemonInfo.type_2]}
+                alt={pokemonInfo.type_2}
+                title={pokemonInfo.type_2}
+              />
+            )}
+          </div>
+        </div>
+        
+        <div className="modal-table1">
+        <h2 className="titulo">Status</h2>
+        <div className="linha-horizontal"></div>
+        <main className="containerDet">
+        <div><button className="bot1">Total</button></div>
+        <div><button className="bot1">{pokemonInfo.total}</button></div>
+        <div><button className="bot2">HP</button></div>
+        <div><button className="bot3">{pokemonInfo.hp}</button></div>
+        <div><button className="bot2">Attack</button></div>
+        <div><button className="bot3">{pokemonInfo.attack}</button></div>
+        <div><button className="bot2">Defense</button></div>
+        <div><button className="bot3">{pokemonInfo.defense}</button></div>
+        <div><button className="bot2">Sp Attack</button></div>
+        <div><button className="bot3">{pokemonInfo.special_attack}</button></div>
+        <div><button className="bot2">Sp Defense</button></div>
+        <div><button className="bot3">{pokemonInfo.special_defense}</button></div>
+        <div><button className="bot2">Speed</button></div>
+        <div><button className="bot3">{pokemonInfo.speed}</button></div>
+        </main>
+        </div>
+        <div className="linha-vertical"></div>
+        <div className="modal-table2">
+      
+        <h2 className="titulo">Ranking Geral</h2>
+        <div className="linha-horizontal"></div>
+        <main className="containerDet">
+        <div><button className="bot1">{pokemonDataCard.Ranking.colocado_total}°</button></div>
+        <div><button className="bot4">{pokemonDataCard.Ranking.colocado_hp}°</button></div>
+        <div><button className="bot4">{pokemonDataCard.Ranking.colocado_attack}°</button></div>
+        <div><button className="bot4">{pokemonDataCard.Ranking.colocado_defense}°</button></div>
+        <div><button className="bot4">{pokemonDataCard.Ranking.colocado_special_attack}°</button></div>
+        <div><button className="bot4">{pokemonDataCard.Ranking.colocado_special_defense}°</button></div>
+        <div><button className="bot4">{pokemonDataCard.Ranking.colocado_speed}°</button></div>
+        </main>
+        </div>
+
+        <div className="modal-footer">
+          <div className="linha-horizontal"></div>
+            <div className="adicionar">+</div>
+              <p className="textFoot">Adicionar à comparação</p>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonDetails;
--- pokeranking/src/Components/PokemonDisplay/index.jsx
+++ pokeranking/src/Components/PokemonDisplay/index.jsx
@@ -1,30 +1,34 @@
+/*
 import { useEffect, useState } from "react";
 import "./style.css";
+import PokemonDetails from "../PokemonDetails";
 
-function PokemonDisplay(props) {
+function PokemonDisplay() {
+  const [pokemonInfo, setPokemonInfo] = useState(null);
   const [pokemonData, setPokemonData] = useState([]);
-  const [displaySize, setDisplaySize] = useState(50);
+  const [modalOpened, setModalOpened] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:5000/criaturas")
       .then((response) => response.json())
-      .then((data) => setPokemonData(data));
+      .then((data) => {setPokemonData(data); console.log(data)});
   }, []);
 
-  const pokemonDataFilter = pokemonData.filter(
-    (e) =>
-      e.name.startsWith(props.pokemonName.toLowerCase()) ||
-      e.codigo == props.pokemonName
-  );
+  function handlePokemonClick(data) {
+    setModalOpened(true);
+    setPokemonInfo(data);
+  }
 
   return (
-    <div className="display-container">
+    <>
+      <PokemonDetails opened={modalOpened} setOpened={setModalOpened} pokemonInfo={pokemonInfo} />
+      
       <ul className="PokeRanking">
-        {pokemonDataFilter
+        {pokemonData
           .map((pokemon) => {
             const liCss = "card " + pokemon.type_1;
             return (
-              <li className={liCss} key={pokemon._id}>
+              <li className={liCss} key={pokemon._id} onClick={() => handlePokemonClick(pokemon)}>
                 <p className="card-title number">#{pokemon.codigo}</p>
                 <img
                   className="card-image"
@@ -35,17 +39,77 @@ function PokemonDisplay(props) {
               </li>
             );
           })
-          .slice(0, displaySize)}
+          .slice(0, 721)}
       </ul>
-      <div className="show-more-container">
-        <button
-          className="show-more"
-          onClick={() => setDisplaySize(displaySize + 50)}
-        >
-          MOSTRAR MAIS
-        </button>
+    </>
+  );
+}
+
+export default PokemonDisplay;
+*/
+
+
+import { useEffect, useState } from "react";
+import "./style.css";
+import PokemonDetails from "../PokemonDetails";
+
+function PokemonDisplay(props) {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [displaySize, setDisplaySize] = useState(50);
+  const [modalOpened, setModalOpened] = useState(false);
+  const [pokemonInfo, setPokemonInfo] = useState(null);
+  
+
+  useEffect(() => {
+    fetch("http://localhost:5000/criaturas")
+      .then((response) => response.json())
+      .then((data) => setPokemonData(data));
+  }, []);
+  
+  function handlePokemonClick(data) {
+    setModalOpened(true);
+    setPokemonInfo(data);
+  }
+
+  const pokemonDataFilter = pokemonData.filter(
+    (e) =>
+      e.name.startsWith(props.pokemonName.toLowerCase()) ||
+      e.codigo == props.pokemonName
+  );
+
+  return (
+    <>
+      <PokemonDetails opened={modalOpened} setOpened={setModalOpened} pokemonInfo={pokemonInfo} />
+      
+      <div className="display-container">
+        <ul className="PokeRanking">
+          {pokemonDataFilter
+            .map((pokemon) => {
+              const liCss = "card " + pokemon.type_1;
+              return (
+                <li className={liCss} key={pokemon._id} onClick={() => handlePokemonClick(pokemon)}>
+                  <p className="card-title number">#{pokemon.codigo}</p>
+                  <img
+                    className="card-image"
+                    src={pokemon.url_image}
+                    alt={pokemon.name}
+                  />
+                  <p className="card-title">{pokemon.name}</p>
+                </li>
+              );
+            })
+            .slice(0, displaySize)}
+        </ul>
+        <div className="show-more-container">
+          <button
+            className="show-more"
+            onClick={() => setDisplaySize(displaySize + 50)}
+          >
+            MOSTRAR MAIS
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
