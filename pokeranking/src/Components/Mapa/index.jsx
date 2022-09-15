789c6dc471f3aaab341dd0bb32773d521b6420c7
JoaoSchmitz <jmatheus12349@gmail.com>
2022-09-15 09:46:42 -0300

    :lipstick: mudanças leves no filtro e mapa

--- pokeranking/src/Components/Mapa/index.jsx
+++ pokeranking/src/Components/Mapa/index.jsx
@@ -14,7 +14,7 @@ function Mapa({ opened, setOpened }) {
         <div className="texto">
           <p>
             <p style={{ fontWeight: 'bold' }}><center>Mapa de regiões Pokémon:</center></p> <br />
-            <p><center>As regiões de Pokémon são divididas em 8 gerações, cada uma com suas características únicas. A primeira geração foi lançada em 1996, e a última em 2019. Cada geração possui 151 Pokémon, sendo que a primeira geração possui 151 Pokémon, a segunda possui 100, a terceira possui 135, a quarta possui 107, a quinta possui 156, a sexta possui 72, a sétima possui 88 e a oitava possui 89.</center></p> <br />
+            <p><center>As regiões de Pokémon são divididas em 8 gerações, cada uma com suas características únicas. A primeira geração foi lançada em 1996, e a última em 2019. Cada geração possui Pokémons diferentes, sendo que a primeira geração possui 151 Pokémon, a segunda possui 100, a terceira possui 135, a quarta possui 107, a quinta possui 156, a sexta possui 72, a sétima possui 88 e a oitava possui 89.</center></p> <br />
             <p><center>No PokéRanking, contamos atualmente com as 6 primeiras gerações. Confira as regiões referentes a cada uma!</center></p><br />
 
             <center><img src={mapa} alt="mapa" /></center> <br />
